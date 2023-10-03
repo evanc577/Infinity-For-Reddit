bd372210b39530de27035e2d0eeef6bd3d411363
Bruno Pimentel <bruno@pimentel.dev>
2023-10-03 17:46:00 -0300

    feat: add input for token and username
    Init apiutils in link resolver
    
    Rework api key input
    
    Fix login

--- app/src/main/java/ml/docilealligator/infinityforreddit/activities/MainActivity.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/activities/MainActivity.java
@@ -210,6 +210,8 @@ public class MainActivity extends BaseActivity implements SortTypeSelectionCallb
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.installSplashScreen(this);
 
+        APIUtils.init(this);
+
         ((Infinity) getApplication()).getAppComponent().inject(this);
 
         setTheme(R.style.AppTheme_NoActionBarWithTransparentStatusBar);
@@ -735,7 +737,7 @@ public class MainActivity extends BaseActivity implements SortTypeSelectionCallb
             }
         });
         navigationWrapper.floatingActionButton.setOnLongClickListener(view -> {
-            FABMoreOptionsBottomSheetFragment fabMoreOptionsBottomSheetFragment= new FABMoreOptionsBottomSheetFragment();
+            FABMoreOptionsBottomSheetFragment fabMoreOptionsBottomSheetFragment = new FABMoreOptionsBottomSheetFragment();
             Bundle bundle = new Bundle();
             bundle.putBoolean(FABMoreOptionsBottomSheetFragment.EXTRA_ANONYMOUS_MODE, accountName.equals(Account.ANONYMOUS_ACCOUNT));
             fabMoreOptionsBottomSheetFragment.setArguments(bundle);
@@ -834,8 +836,8 @@ public class MainActivity extends BaseActivity implements SortTypeSelectionCallb
                             startActivity(intent);
                             finish();
                         });
-                    }
-                });
+            }
+        });
         adapter.setInboxCount(inboxCount);
         binding.navDrawerRecyclerViewMainActivity.setLayoutManager(new LinearLayoutManagerBugFixed(this));
         binding.navDrawerRecyclerViewMainActivity.setAdapter(adapter.getConcatAdapter());
@@ -1022,11 +1024,11 @@ public class MainActivity extends BaseActivity implements SortTypeSelectionCallb
                     }
                 }
 
-                @Override
-                public void onFetchUserDataFailed() {
-                    mFetchUserInfoSuccess = false;
-                }
-            });
+                        @Override
+                        public void onFetchUserDataFailed() {
+                            mFetchUserInfoSuccess = false;
+                        }
+                    });
             /*FetchMyInfo.fetchAccountInfo(mOauthRetrofit, mRedditDataRoomDatabase, mAccessToken,
                     new FetchMyInfo.FetchMyInfoListener() {
                         @Override
--- app/src/main/java/ml/docilealligator/infinityforreddit/videoautoplay/ToroExo.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/videoautoplay/ToroExo.java
@@ -211,6 +211,6 @@ public final class ToroExo {
 
     @SuppressWarnings("SameParameterValue")
     private static String getUserAgent() {
-        return APIUtils.USER_AGENT;
+        return APIUtils.getUserAgent();
     }
 }
